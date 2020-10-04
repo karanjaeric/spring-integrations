ae6f6fbc10658971a60851b75e13aaa73d54f1c0
Eric Karanja <ekaranja@cellulant.com>
2020-10-04 17:00:25 +0300

    publish-subscribe channel,task executor

--- src/main/java/com/rnd/springintegrations/PrintService.java
+++ src/main/java/com/rnd/springintegrations/PrintService.java
@@ -2,13 +2,11 @@ package com.rnd.springintegrations;
 
 
 import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 public class PrintService {
 
 	public void print(Message<String> message) {
 		
-//		System.out.println("Message Payload "+message.getPayload());
-		throw new RuntimeException("This is an error");
+		System.out.println("Message Payload "+message.getPayload());
 	}
 
 }
